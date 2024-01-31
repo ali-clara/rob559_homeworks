9ea0a531e8d15bbc1f28d2d43f36ec2c17a3bf87
Ali <jonesal9@oregonstate.edu>
2024-01-31 10:16:58 -0800

    adding hw1

---
+++ rob599_hw1/src/drive_forward.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+# Drives the Fetch forward until it's 1m away from the nearest obstacle
+# subscribes to laser rangefinder messages (type LaserScan)
+# publishes messages (type Twist) to cmd_vel
+
+# import the ROS stuff
+import rospy
+import sys
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+# import the other stuff
+import numpy as np
+
+class DriveForward():
+    def __init__(self) -> None:
+        # Initialize the node
+        rospy.init_node('mover', argv=sys.argv)
+        
+        # Set up the publisher and subscriber. 
+        # The Fetch will listen for Twist messages on the cmd_vel topic.
+        rospy.loginfo("Initializing drive-to-wall publisher and subscriber")
+        self.publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.subscriber = rospy.Subscriber('base_scan', LaserScan, self.laser_callback)
+
+        self.laser_ranges = None
+        self.laser_angles = None
+
+        self.range_goal = None
+        self.angle_goal = None
+
+    def laser_callback(self, msg):
+        """Callback function for the laser subscriber. Called every time a new
+            message (type LaserScan) is recieved.
+            Saves the angle range and the laser range to class variables as np arrays"""
+        self.laser_angles = np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)
+        self.laser_ranges = np.array(msg.ranges)
+        # rospy.loginfo(f"Laser scan: min distance {min(msg.ranges)}")
+
+    def find_goal(self):
+        """Finds the x distance and angle goal in the Fetch's coordinate frame"""
+        min_laser_index = np.argmin(self.laser_ranges)
+        self.angle_goal = self.laser_angles[min_laser_index]
+        # rospy.loginfo(f"Minimum distance found {self.angle_goal}")
+        self.range_goal = self.laser_ranges[min_laser_index]
+        rospy.loginfo(f"Closest laser scan: {np.round(self.range_goal,3)}m at {np.round(np.rad2deg(self.angle_goal),3)}deg")
+    
+    def calculate_velocity(self): 
+        """Maps the x distance (range) goal and the angle goal to velocities"""
+        x_distance_between = [1, 5]   # as the distance ranges between 1-5m,
+        x_velocity_between = [0, 1]   # linearly vary the velocity between 0-1m/s
+        x_velocity = np.interp(self.range_goal, x_distance_between, x_velocity_between)
+
+        z_angle_between = [-1.92, 1.92] # as the angle ranges from -1.92-1.92 rad,
+        z_velocity_bewteen = [-0.15*(2*np.pi), 0.15*(2*np.pi)] # linearly vary the velocity between -0.5-0.5rev/s
+        z_velocity = np.interp(self.angle_goal, z_angle_between, z_velocity_bewteen)
+
+        return x_velocity, z_velocity
+        
+    def create_fetch_twist(self, linear_x, angular_z):
+        """Formats a Twist message for the Fetch, filling in the linear x velocity
+            and the angular z velocity"""
+        cmd = Twist()
+
+        # Linear velocities: m/s
+        cmd.linear.x = min(linear_x, 1) # make sure we cap at 1m/s
+        cmd.linear.y = 0.0
+        cmd.linear.z = 0.0
+
+        # Rotational velocities: rad/sec 
+        # The Fetch will only respond to rotations around the z (vertical) axis
+        cmd.angular.x = 0.0
+        cmd.angular.y = 0.0
+        cmd.angular.z = min(angular_z, 0.25*(2*np.pi))   # make sure we cap at 0.25 rev/s
+
+        return cmd
+    
+    def run(self):
+        """Runs the node"""
+        rate = rospy.Rate(10)
+        # hang out until we've recieved a message
+        rospy.wait_for_message('base_scan', LaserScan, timeout=10)
+        while not rospy.is_shutdown():
+            self.find_goal()
+            x_velocity, z_velocity = self.calculate_velocity()
+            cmd = self.create_fetch_twist(x_velocity, z_velocity)
+            rospy.loginfo(f"Publishing {np.round(cmd.linear.x,3)}m/s, {np.round(cmd.angular.z,3)}rad/s")
+            self.publisher.publish(cmd)
+
+            rate.sleep()
+
+if __name__ == "__main__":
+    my_drive = DriveForward()
+    my_drive.run()
